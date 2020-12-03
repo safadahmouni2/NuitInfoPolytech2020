a70db975317a33e0cf1e6d915f236284665a2246
polytech2019 <dahmounisafa3@gmail>
2020-12-03 21:14:06 +0100

    fisrt commit

---
+++ nuitinfo/nuitinfo/src/main/java/com/nuitinfo/NuitinfoApplication.java
@@ -0,0 +1,13 @@
+package com.nuitinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuitinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuitinfoApplication.class, args);
+	}
+
+}
